fa136d45e3d7dc51e969a62863e926c49a349f15
danovs <dsludanov@gmail.com>
2024-09-11 18:11:40 +0300

    Добавьте файлы проекта.

---
+++ AnotherDLL/dllmain.cpp
@@ -0,0 +1,9 @@
+﻿// testfunc.cpp: определяет экспортированную функцию для библиотеки DLL.
+
+#include "pch.h"
+
+extern "C" __declspec(dllexport) void __cdecl TestHello(void)
+{
+    MessageBox(NULL, L"Hello, you're calling a function in this DLL", L"DLL Skeleton", MB_OK);
+    return;
+}
